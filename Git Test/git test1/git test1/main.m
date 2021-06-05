ed63320f3ca67052c51b301c6b10ba029fe5b180
BillyMiracle <510986792@qq.com>
2021-06-05 19:45:36 +0800

    Test1

---
+++ Git Test/git test1/git test1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git test1
+//
+//  Created by 张博添 on 2021/6/5.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
